7a8b0c6c294c09c1fa9c21e6c140661f1ad6df99
Haowen Yong <heyhw@csu.fullerton.edu>
2020-09-16 19:35:43 -0700

    added previous and next button

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,14 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var PREVIOUS_SELECTOR = '[button-role="previous pic"]';
+var NEXT_SELECTOR = '[button-role="next pic"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
+
+var thumb_array = getThumbnailsArray();
+var i = 0;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -30,10 +35,17 @@ function setDetailsFromThumb(thumbnail) {
     setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
+function assignIterator(thumb) {
+    'use strict';
+    thumb.classList.add(i);
+    i = i + 1;
+}
+
 function addThumbClickHandler(thumb) {
     'use strict';
-    thumb.addEventListener('click', function(event) {
+    thumb.addEventListener('click', function (event) {
         event.preventDefault();
+        i = parseInt(thumb.class);
         setDetailsFromThumb(thumb);
         showDetails();
     });
@@ -61,6 +73,40 @@ function showDetails() {
     }, 50);
 }
 
+function previousItem() {
+    'use strict';
+    if (i === 0) {
+        i = thumb_array.length;
+    }
+    i = i - 1;
+    return thumb_array[i];
+}
+
+function nextItem() {
+    'use strict';
+    i = i + 1;
+    i = i % thumb_array.length;
+    return thumb_array[i];
+}
+
+function addPreviousPressHandler() {
+    'use strict';
+    var previous = document.querySelector(PREVIOUS_SELECTOR);
+    previous.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(previousItem());
+    });
+}
+
+function addNextPressHandler() {
+    'use strict';
+    var next = document.querySelector(NEXT_SELECTOR);
+    next.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(nextItem());
+    });
+}
+
 function addKeyPressHandler() {
     'use strict';
     document.body.addEventListener('keyup', function (event) {
@@ -75,7 +121,11 @@ function addKeyPressHandler() {
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(assignIterator);
+    i = 0;
     thumbnails.forEach(addThumbClickHandler);
+    addPreviousPressHandler();
+    addNextPressHandler();
     addKeyPressHandler();
 }
 
